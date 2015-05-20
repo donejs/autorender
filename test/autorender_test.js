df355359282807b9f2ebddd0f7545c117b5d442f
Matthew Phillips <matthew@matthewphillips.info>
2015-05-20 16:06:35 -0400

    Use an iframe instead of F.open
    F.open doesn't work in travis

--- test/autorender_test.js
+++ test/autorender_test.js
@@ -1,17 +1,20 @@
 var QUnit = require("steal-qunit");
+var $ = require("jquery");
 var F = require("funcunit");
 
 F.attach(QUnit);
 
 QUnit.module("done-autorender",{
 	setup: function(){
-		F.open("//progressive.html");
+		$("#qunit-fixture").html(
+			$("<iframe src='progressive.html' name='f'></iframe>")
+		);
 	}
 });
 
 
 
 QUnit.test("basics works", function(){
-	F("order-history").exists("order-history element is on the page");
-	F("[asset-id]").size(2, "There are 2 style tags on the page");
+	F("order-history", "f").exists("order-history element is on the page");
+	F("[asset-id]", "f").size(2, "There are 2 style tags on the page");
 });

be32d764cae9954356b48ee12242b41971033b44
Manuel Mujica <manuel.expat@gmail.com>
2018-01-05 12:29:40 -0700

    Register dynamic imports in the slim loader config

--- test/basics-optimized/state.js
+++ test/basics-optimized/state.js
@@ -1,7 +1,12 @@
 define(["can-map", "can-route"], function(Map) {
+	if (!window.steal.map["test/basics-optimized/state"]) {
+		throw new Error("can-import modules should be registered");
+	}
+
 	return Map.extend({
-		hello: function(){
+		hello: function() {
 			return "world";
 		}
 	});
 });
+
